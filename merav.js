8447c1ed287002c1adc3df1e3dfdb3263a0384c1
merav3 <ninameravifrach@gmail.com>
2022-08-17 16:33:37 +0300

    first commit

---
+++ merav.js
@@ -0,0 +1,29 @@
+console.log("hello merav")
+let name="ori"
+console.log(name)
+console.log("hi david")
+let x="david"
+console.log(x)
+console.log("anna")
+let q="anna"
+console.log(q)
+let y=3
+console.log(y)
+let r="yona"
+console.log(r)
+let w="david"
+console.log(w)
+let x1=3000
+let x2=927
+console.log(x1+x2)
+let a1=1000
+let a2=550
+console.log(a1-a2)
+let m1=2200
+let m2=200
+let sum=m1+m2
+console.log(sum)
+let l1=300
+let l2=50
+let s=l1-l2
+console.log(s)
